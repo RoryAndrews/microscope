9d8fc94a0f57ccd54f96318e465fe69ec83d23e3
Rory Andrews <rory00@gmail.com>
2015-10-29 19:33:02 -1000

    Finished chapter 4

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,20 +1,6 @@
 /**
  * Created by Rory on 10/29/2015.
  */
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  }
-];
 Template.postsList.helpers({
   posts: function() {
     return Posts.find();
