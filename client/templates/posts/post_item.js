2d39e5a5bc5c90c0b255474dce72f4037e0d0a3d
Rory Andrews <rory00@gmail.com>
2015-11-02 21:22:42 -1000

    Commit 8-1
    Added edit posts form.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,6 +2,9 @@
  * Created by Rory on 10/29/2015.
  */
 Template.postItem.helpers({
+  ownPost: function() {
+    return this.userId === Meteor.userId();
+  },
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url;
